861a0eefaa9b27121a49827fcd350628742d24c7
aep <aepsaprul18@gmail.com>
2023-02-14 16:13:09 +0700

    produk detail sementara

---
+++ resources/views/layouts/headerArrow.blade.php
@@ -0,0 +1,29 @@
+<div id="header-wrapper" class="w-full h-12 border-b flex justify-center items-center bg-white sticky top-0">
+  <div class="w-full flex justify-between items-center">
+    <a href="{{ url('/') }}"><i class="fa fa-arrow-left pl-3 text-xl text-slate-500"></i></a>
+    <div class="relative mx-1">
+      <input type="text" name="cari" id="cari" placeholder="Cari produk" class="h-8 w-64 pl-3 border rounded-3xl text-sm">
+      <i class="fa fa-search absolute right-3 top-1.5 text-slate-400 text-sm"></i>
+    </div>
+    <i class="fa fa-basket-shopping mx-1 text-xl"></i>
+    <i id="menu" class="fa fa-bars mr-2 text-xl"></i>
+  </div>
+</div>
+
+<script>
+  // menu
+  document.getElementById('menu').onclick = function() {
+    bukaNavigasi();
+  }
+  document.getElementById('btn_close').onclick = function() {
+    tutupNavigasi();
+  }
+
+  function bukaNavigasi() {
+    document.getElementById("menu_id").style.width = "100%";
+  }
+
+  function tutupNavigasi() {
+    document.getElementById("menu_id").style.width = "0%";
+  }
+</script>
--- resources/views/produkDetail.blade.php
+++ resources/views/produkDetail.blade.php
@@ -2,17 +2,56 @@
 
 @section('content')
 
-@include('layouts.header')
+@include('layouts.headerArrow')
 
 <div>
   <div>
     <img src="{{ $produk->gambar }}" alt="">
   </div>
-  <div>Rp. <span class="harga">{{ $produk->harga }}</span></div>
+  <div class="mx-3 my-2">
+    <div class="my-3 text-xl font-bold">{{ $produk->nama }}</div>
+    <div>
+      <p class="text-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis sequi ea iusto ipsum? Sequi, voluptates corporis nihil aperiam laborum quibusdam!</p>
+    </div>
+    <div class="mt-4">
+      <label for="keterangan" class="font-semibold">Keterangan</label>
+      <textarea name="keterangan" id="keterangan" rows="4" class="w-full border border-sky-600 rounded p-3 mt-2 outline-0" placeholder="Maksimal 200 karakter"></textarea>
+    </div>
+    <div class="mt-4">
+      <div class="flex justify-between py-1">
+        <div>Harga Barang</div>
+        <div>Rp 95.000</div>
+      </div>
+      <div class="flex justify-between py-1">
+        <div>Jumlah</div>
+        <div>100 pcs</div>
+      </div>
+      <div class="flex justify-between py-1">
+        <div class="font-bold">Total</div>
+        <div>Rp 105.000</div>
+      </div>
+    </div>
+    <div class="mt-5">
+      <div class="tab grid grid-cols-2">
+        <button id="btn-rincian" class="linktab rounded-tl py-1" id="default">Rincian</button>
+        <button id="btn-ulasan" class="linktab rounded-tr py-1">Ulasan</button>
+      </div>
+      
+      <div id="rincian" class="mt-3">
+        <h3>Rincian</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, molestiae.</p>
+      </div>      
+      <div id="ulasan" class="hidden mt-3">
+        <h3>Ulasan</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptatibus in labore iusto autem dolorum sed. Exercitationem ducimus natus expedita.</p> 
+      </div>
+    </div>
+  </div>
+  <div class="w-full fixed bottom-0 bg-white border-t flex">
+    <button class="w-full m-3 p-1 rounded border-2 border-sky-600 font-bold">Beli</button>
+    <button class="w-full bg-sky-600 m-3 p-1 rounded text-white font-bold"><i class="fa fa-plus"></i> Keranjang</button>
+  </div>
 </div>
-
-
-@include('layouts.navBawah')
 
 @endsection
 
@@ -20,7 +59,6 @@
 <script type="module">
   $(document).ready(function () {
     const harga = $('.harga').text();
-    console.log(afRupiah(harga));
     $('.harga').html(afRupiah(harga))
     
     function afRupiah(bilangan) {
@@ -36,7 +74,41 @@
 
       return rupiah;
     }
-  })
 
+    // tab detail
+    const tab = [];
+
+    $('#btn-ulasan').on('click', function (e) {
+      e.preventDefault();
+      tab.pop();
+      tab.push('ulasan');
+    
+      if (tab[0] == 'ulasan') {
+        $('#btn-ulasan').removeClass('bg-slate-100');
+        $('#btn-ulasan').addClass('bg-sky-500 text-white');
+        $('#btn-rincian').removeClass('bg-slate-100 text-white');
+        $('#btn-rincian').addClass('bg-slate-100');
+
+        $('#rincian').addClass('hidden');
+        $('#ulasan').removeClass('hidden');
+      }
+    })
+    $('#btn-rincian').on('click', function (e) {
+      e.preventDefault();
+      tab.pop();
+    
+      if (tab[0] != 'ulasan') {
+        $('#btn-ulasan').addClass('bg-slate-100');
+        $('#btn-ulasan').removeClass('bg-sky-500 text-white');
+        $('#btn-rincian').removeClass('bg-slate-100');
+        $('#btn-rincian').addClass('bg-sky-500 text-white');
+
+        $('#rincian').removeClass('hidden');
+        $('#ulasan').addClass('hidden');
+      }
+    })
+    $('#btn-rincian').addClass('bg-sky-500 text-white');
+    $('#btn-ulasan').addClass('bg-slate-100');
+  })
 </script>
 @endsection
